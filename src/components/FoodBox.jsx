c188bec4af27097414ad763c1df1f6b56a7fb98d
Oscar Miras <oscar.miras@gmail.com>
2023-07-12 17:20:17 +0200

    Iteration 2

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,19 @@
 // Your code here
+export default function FoodBox({ food }) {
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+                <img src={food.image} alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{food.name}</h2>
+                <p><strong>Calories: </strong>{food.calories}</p>
+                <p><strong>Servings: </strong>{food.servings}</p>
+
+                <div className="card-actions">
+                    <button className="btn btn-primary">Delete</button>
+                </div>
+            </div>
+        </div>
+    );
+}
